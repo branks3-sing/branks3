2829a280aee840e865a8c6b97db49c1ab03e434d
branks3-sing <branks3.sing@gmail.com>
2026-03-01 13:48:21 +0530

    Update urls.py

--- recorder/urls.py
+++ recorder/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('toggle_share/<int:song_id>/', views.toggle_share, name='toggle_share'),
     path('upload_song/', views.upload_song, name='upload_song'),
     path('upload_recording/<int:song_id>/', views.upload_recording, name='upload_recording'),
-    # NEW: song ID + slug – ఇదే మనకు కావాల్సిన format
+    # NEW: song ID + slug – format
     path('<int:song_id>/<slug:slug>/', views.karaoke_player, name='song_detail'),
+    path('delete_song/<int:song_id>/', views.delete_song, name='delete_song'),
 ]
